846bfac9e1363808f79a6748c650f9993db27c29
lantran12 <lantran.labety@gmail.com>
2026-06-22 18:44:15 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,5 +3,11 @@ const GENDERS = [
     { id: "tien-hiep", name: "Tiên hiệp" },
     { id: "guong-vo", name: "Gương vỡ lại lành" },
     { id: "trinh-tham", name: "Trinh thám" },
-    { id: "hai-huoc", name: "Hài hước" }
+    { id: "hai-huoc", name: "Hài hước" },
+    { id: "dep-manh", name: "Đẹp - Mạnh - Khổ" },
+    { id: "ngot-ngao", name: "Ngọt ngào" },
+    { id: "ban-chich", name: "Bạn chịch" },
+    { id: "only-love", name: "Yêu một mình em" },
+    { id: "co-trang", name: "Cổ trang" },
+    { id: "xuyen-nhanh", name: "Xuyên nhanh" }
 ];
